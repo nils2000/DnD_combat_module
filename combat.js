6ce1b673a69b8a806d8b9bea6203c3fdf2024788
unknown <C:\Users\Holger Schuh\AppData\Roaming\The Bat!>
2019-12-08 21:42:42 +0100

    displaying values started (incomplete)

--- combat.js
+++ combat.js
@@ -1,13 +1,29 @@
 var combatant_names;
-// Begin Test setup
 combatant_names = ["Hero", "Monster"];
 var combatant_selector = get_element("add_combatant");
-combatant_selector.appendChild(selection_menue(combatant_names));
-// End Test setup
+combatant_selector.appendChild(selection_menu(combatant_names));
+function named_input(name, size, content) {
+    if (content === void 0) { content = ""; }
+    var input = document.createElement("input");
+    input.id = name;
+    input.size = size;
+    input.name = name;
+    input.value = content;
+    return input;
+}
+function labeled_input(name, size, content) {
+    if (content === void 0) { content = ""; }
+    var label = document.createElement("label");
+    var input = named_input(name, size, content);
+    label.htmlFor = name;
+    label.innerText = name;
+    label.appendChild(input);
+    return label;
+}
 function get_element(id) {
     return document.getElementById(id);
 }
-function selection_menue(values) {
+function selection_menu(values) {
     var selectList = document.createElement("select");
     for (var i = 0; i < values.length; i++) {
         var option = document.createElement("option");
@@ -17,5 +33,39 @@ function selection_menue(values) {
     }
     return selectList;
 }
+function named_textarea(name, rows) {
+    var span = document.createElement("span");
+    var textarea = document.createElement("textarea");
+    span.title = name;
+    textarea.id = name;
+    textarea.innerText = name;
+    textarea.rows = rows;
+    span.appendChild(textarea);
+    return span;
+}
+//https://stackoverflow.com/questions/3450593/how-do-i-clear-the-content-of-a-div-using-javascript
+function delete_children(parent_element) {
+    while (parent_element.firstChild) {
+        parent_element.removeChild(parent_element.firstChild);
+    }
+}
 var monster_names = monsters.map(function (e) { return e.Name; });
-combatant_selector.appendChild(selection_menue(monster_names));
+var monster_selector = selection_menu(monster_names);
+combatant_selector.appendChild(monster_selector);
+monster_selector.addEventListener("change", display_monster);
+function get_monster(name) {
+    return monsters.filter(function (e) { return e.Name == name; })[0];
+}
+function display_monster(ev) {
+    var display = get_element("combatant_description");
+    delete_children(display);
+    //console.log(ev.target.value);
+    var monster = get_monster(ev.target.value);
+    var kys = Object.keys(monster);
+    kys.forEach(function (k) {
+        var val = monster[k];
+        var size = val.length;
+        console.log(k, val);
+        display.appendChild(labeled_input(k, size, val));
+    });
+}
--- combat.ts
+++ combat.ts
@@ -1,22 +1,34 @@
 declare var monsters: any;
 var combatant_names: string[];
 
-// Begin Test setup
-
 combatant_names = ["Hero", "Monster"];
 var combatant_selector = get_element("add_combatant");
-combatant_selector.appendChild(selection_menue(combatant_names));
+combatant_selector.appendChild(selection_menu(combatant_names));
 
-// End Test setup
+function named_input(name: string, size: number, content: string = "") {
+    let input = document.createElement("input");
+    input.id = name;
+    input.size = size;
+    input.name = name;
+    input.value = content;
+    return input;
+}
+
+function labeled_input(name: string, size: number, content: string = "") {
+    let label = document.createElement("label");
+    let input = named_input(name, size, content);
+    label.htmlFor = name;
+    label.innerText = name;
+    label.appendChild(input);
+    return label;
+}
 
 function get_element(id: string) {
     return document.getElementById(id);
 }
 
-function selection_menue(values: string[]) {
+function selection_menu(values: string[]) {
     var selectList = document.createElement("select");
-
-
     for (var i = 0; i < values.length; i++) {
         var option = document.createElement("option");
         option.value = values[i];
@@ -26,5 +38,47 @@ function selection_menue(values: string[]) {
     return selectList;
 }
 
+function named_textarea(name: string, rows: number) {
+    let span = document.createElement("span");
+    let textarea = document.createElement("textarea");
+    span.title = name;
+    textarea.id = name;
+    textarea.innerText = name;
+    textarea.rows = rows;
+    span.appendChild(textarea);
+    return span;
+}
+
+//https://stackoverflow.com/questions/3450593/how-do-i-clear-the-content-of-a-div-using-javascript
+function delete_children(parent_element) {
+    while (parent_element.firstChild) {
+        parent_element.removeChild(parent_element.firstChild);
+    }
+}
+
 var monster_names: string[] = monsters.map(e => e.Name);
-combatant_selector.appendChild(selection_menue(monster_names));
+var monster_selector = selection_menu(monster_names);
+combatant_selector.appendChild(monster_selector);
+monster_selector.addEventListener("change", display_monster);
+
+
+function get_monster(name: String) {
+    return monsters.filter(e => e.Name == name)[0];
+}
+
+function display_monster(ev: any) {
+    var display = get_element("combatant_description");
+    delete_children(display);
+    //console.log(ev.target.value);
+
+    var monster = get_monster(ev.target.value);
+    var kys = Object.keys(monster);
+    kys.forEach(k => {
+        //entweder das Feld enthält einen String oder ein Objekt
+        var val: string = monster[k];
+        var size = val.length;
+        console.log(k, val);
+        display.appendChild(labeled_input(k, size, val));
+    }
+    );
+}
